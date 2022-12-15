9b0894f508ed90572631029086778c0f72d999a6
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2022-12-15 18:44:53 +0700

    modify selector to get follow button

--- index.js
+++ index.js
@@ -57,48 +57,63 @@ function delay(time) {
     const url = page.url();
     if (url == 'https://twitter.com/home') {
         console.log('login berhasil')
-        await page.goto('https://twitter.com/explore');
-        const inputSearchKey = 'input[data-testid=SearchBox_Search_Input]';
-        await page.waitForSelector(inputSearchKey);
-        await page.type(inputSearchKey, searchKey);
-        await page.keyboard.press('Enter');
-        console.log('menuju url pencarian mutual')
-        await delay(5000)
-        let data = []
-        data.push(...await page.evaluate(
-            () => Array.from(
-                document.querySelectorAll('a.css-4rbku5.css-18t94o4.css-901oao.r-1bwzh9t.r-1loqt21.r-xoduu5.r-1q142lx.r-1w6e6rj.r-37j5jr.r-a023e6.r-16dba41.r-9aw3ui.r-rjixqe.r-bcqeeo.r-3s2u2q.r-qvutc0'),
-                a => a.href
-            )
-        ));
-        console.log('mendapatkan semua link mutual')
-        console.log(data)
-        let rd = data.reverse();
-        for (const el of rd) {
-            await page.goto(el);
-            await delay(5000);
-            const tweetParent = await page.$('article[data-testid=tweet]')
-            await tweetParent.click()
-            await delay(3000);
-            const urlLike = page.url();
-            console.log('menuju link untuk follow-follow orang')
-            await page.goto(`https://twitter.com/${urlLike.split("/")[3]}/status/${urlLike.split("/")[5]}/likes`);
-            await delay(10000);
-            const followButton = await page.$$('div.css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-15ysp7h.r-4wgw6l.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr')
-            for (const el of followButton) {
+        for (let i = 0; i < 10; i++) {
+            let search = true
+            let data = []
+            while (search) {
 
-                await el.click()
-                console.log(chalk.greenBright('sukses follow'))
-                console.log('tunggu 10 detik sebelum menfollow akun selanjutnya')
-                await delay(10000);
+
+                await page.goto('https://twitter.com/explore');
+                const inputSearchKey = 'input[data-testid=SearchBox_Search_Input]';
+                await page.waitForSelector(inputSearchKey);
+                await page.type(inputSearchKey, searchKey);
+                await page.keyboard.press('Enter');
+                console.log('menuju url pencarian mutual')
+                await delay(5000)
+
+                data.push(...await page.evaluate(
+                    () => Array.from(
+                        document.querySelectorAll('a.css-4rbku5.css-18t94o4.css-901oao.r-1bwzh9t.r-1loqt21.r-xoduu5.r-1q142lx.r-1w6e6rj.r-37j5jr.r-a023e6.r-16dba41.r-9aw3ui.r-rjixqe.r-bcqeeo.r-3s2u2q.r-qvutc0'),
+                        a => a.href
+                    )
+                ));
+                if (data.length > 1) {
+                    console.log('mendapatkan semua link mutual')
+                    console.log(data)
+                    search = false
+                }
+
             }
-            console.log('menunggu 10 menit biar ga banned')
-            await delay(600000)
+            let rd = data.reverse();
+            for (const el of rd) {
+                await page.goto(el);
+                await delay(5000);
+                const tweetParent = await page.$('article[data-testid=tweet]')
+                await tweetParent.click()
+                await delay(3000);
+                const urlLike = page.url();
+                console.log('menuju link untuk follow-follow orang')
+                await page.goto(`https://twitter.com/${urlLike.split("/")[3]}/status/${urlLike.split("/")[5]}/likes`);
+                await delay(5000);
+
+                let tempFollowButton = await page.$$('.r-rs99b7')
+                console.log(tempFollowButton)
+                const followButton = tempFollowButton.slice(1)
+                console.log(followButton)
+                for(const el of followButton) {
+                    await delay(3000);
+                    await el.click()
+                    console.log(chalk.greenBright('sukses follow'))
+                    console.log('tunggu 10 detik sebelum menfollow akun selanjutnya')
+                    await delay(10000);
+                }
+                console.log('menunggu 10 menit biar ga banned')
+                await delay(600000)
+
+            }
+            console.log('link sudah habis, mencoba mencari kembali....')
 
         }
-        console.log('link sudah habis, mencoba mencari kembali....')
-
-
 
     }
     console.log('gagal login, password salah!')
