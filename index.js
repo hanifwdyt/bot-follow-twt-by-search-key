99ac825ce5d70e512513a69bca6c2fffd1f570c8
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2023-01-11 17:32:32 +0700

    add random delay time when follow each person

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const userAgent = new UserAgent();
 
 
 //get Username Twitter
-let user = readlineSync.question(chalk.blue('username twitter? '));
+let user = readlineSync.question(chalk.blue('username twitter: '));
 
 // get Password Twitter
-let pass = readlineSync.question(chalk.red('password twitter?  '), {
+let pass = readlineSync.question(chalk.red('password twitter:  '), {
     hideEchoBack: true // The typed text on screen is hidden by `*` (default).
 });
 
 //get Search Keyword
-let searchKey = readlineSync.question(chalk.green('search keyword? (ex:autofollback mutualan, ayo mutualan)  '));
+let searchKey = readlineSync.question(chalk.green('search keyword: (ex:autofollback mutualan, ayo mutualan)  '));
 
 function delay(time) {
     return new Promise(function (resolve) {
@@ -37,7 +37,7 @@ const time = () => {
             // headless:false
             executablePath: executablePath()
         });
-    
+
         const page = await browser.newPage();
         await page.setExtraHTTPHeaders({
             'Accept-Language': 'en-US,en;q=0.9'
@@ -66,17 +66,31 @@ const time = () => {
         const url = page.url();
         if (url == 'https://twitter.com/home') {
 
-             // numpang follow owner nya ya, mau dihapus jg gpp kok wkwk
+            // numpang follow owner nya ya, mau dihapus jg gpp kok wkwk
             await page.goto('https://twitter.com/depokjkt/status/1604507843595419648/likes')
             console.log(`[${time()}] login berhasil!`)
             if (await page.$('div.css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-15ysp7h.r-4wgw6l.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr') !== null) {
                 const followOwner = await page.$('div.css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-15ysp7h.r-4wgw6l.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr')
-                await page.evaluate(e=>e.click(), followOwner)
-            } 
-           
-          
-           
-            
+                await page.evaluate(e => e.click(), followOwner)
+		console.log('ok')
+            }
+
+            const getInfoAccount = async () => {
+                await page.goto('https://twitter.com/' + user)
+                await page.waitForSelector('div.css-1dbjc4n > a > span')
+                let infoTempt = []
+                infoTempt.push(...await page.evaluate(
+                    () => Array.from(
+                        document.querySelectorAll('div.css-1dbjc4n > a > span'),
+                        e => e.innerHTML
+                    )
+                ))
+                let following = infoTempt[0].split('r-bcqeeo r-qvutc0">')[1].split('</span>')
+                let followers = infoTempt[2].split('r-bcqeeo r-qvutc0">')[1].split('</span>')
+                    
+                return console.log(`[${time()}] jumlah following saat ini: ${following} \n[${time()}] jumlah followers saat ini: ${followers}`)
+            }
+            await getInfoAccount()
             for (let i = 0; i < 10; i++) {
                 let search = true
                 let data = []
@@ -103,7 +117,7 @@ const time = () => {
 
                 }
                 let rd = data.reverse();
-                for (const el of data) {
+                for (const el of rd) {
                     await page.goto(el);
                     await delay(5000);
 
@@ -131,13 +145,16 @@ const time = () => {
                             i = followButton.length
                         }
                         console.log(`[${time()}] ${chalk.greenBright('sukses follow!')}`)
-                        console.log(`[${time()}] tunggu 10 detik sebelum menfollow akun selanjutnya.`)
-                        await delay(7000);
+                        const delayFollow = Math.floor(Math.random()*15) + 5
+                        console.log(`[${time()}] tunggu ${delayFollow} detik sebelum menfollow akun selanjutnya.`)
+                        await delay(delayFollow * 1000);
                     }
                     console.log(`[${time()}] berhasil memfollow ${followButton.length} orang`)
                     console.log(`[${time()}] menunggu 10 menit biar ga banned`)
                     console.log(`[${time()}] demi keamanan, jangan akses twitter dengan browser lain ketika posisi sedang login dari BOT ini!`)
-                    await delay(600000)
+                    await delay(595000)
+                    await getInfoAccount()
+                    await delay(5000)
 
                 }
                 console.log(`[${time()}] link sudah habis, mencoba mencari kembali...`)
